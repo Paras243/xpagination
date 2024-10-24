bf1af427ec850cfc3d7fde49d45029ed1744c845
Paras_Raina <parasr243@gmail.com>
2024-10-24 13:29:04 +0530

    udated code

--- src/pagination.jsx
+++ src/pagination.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import "./page.css";
 
 const PaginationApp = () => {
@@ -78,6 +78,95 @@ const PaginationApp = () => {
         </button>
       </div>
 
+    </div>
+  );
+};
+
+export default PaginationApp;
+*/
+
+import React, { useState, useEffect } from "react";
+import "./page.css";
+
+const PaginationApp = () => {
+  const [employees, setEmployees] = useState([]); 
+  const [currentPage, setCurrentPage] = useState(1); 
+  const rowsPerPage = 10; 
+
+  const fetchEmployees = async () => {
+    try {
+      const response = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+      if (response.ok) {
+        const data = await response.json(); 
+        setEmployees(data); 
+      } else {
+        alert("Failed to fetch data"); 
+      }
+    } catch (error) {
+      alert("Failed to fetch data"); 
+    }
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const indexOfLastEmployee = currentPage * rowsPerPage; 
+  const indexOfFirstEmployee = indexOfLastEmployee - rowsPerPage; 
+  const currentEmployees = employees.slice(indexOfFirstEmployee, indexOfLastEmployee); 
+
+  const nextPage = () => {
+    if (currentPage < Math.ceil(employees.length / rowsPerPage)) {
+      setCurrentPage(currentPage + 1); 
+    } else {
+      return null; 
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1); 
+    } else {
+      return null; 
+    }
+  };
+
+  return (
+    <div className="container-div">
+      <h1>Employee Data Table</h1>
+
+      <table className="rows" border="1" cellPadding="10" cellSpacing="0">
+        <thead style={{color:"antiquewhite",backgroundColor:"green"}}>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentEmployees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        <button className="button" onClick={prevPage}>
+          Previous
+        </button>
+        <span style={{ margin: "0 10px", width: "20px", height: "40px" }} id="page-number">
+          {currentPage}
+        </span>
+        <button className="button" onClick={nextPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
